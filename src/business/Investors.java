28ec25a1f641e11403b6883ab743b2c13d9a1cc9
Leandro Silveira <33546260+Silveiraleandro@users.noreply.github.com>
2020-05-11 12:15:22 +0100

    class Investors done
    Class created using the builder pattern
    and a method random Budget

--- src/business/Investors.java
+++ src/business/Investors.java
@@ -9,3 +9,75 @@ public class Investors {
     private String id;
     private int numberOfShares;
     private double budget;
+
+    /*Defining a constructor that have the object builder bringing
+       all the attributes that the Country has
+        */
+    private Investors(BuilderInvestors buildInv){
+
+        this.id = buildInv.id;
+        this.budget = buildInv.ramdomBudget();
+
+    }
+    /*
+        Getters and Setters
+         */
+    public String getId() {
+        return id;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+    /*
+   this method is the one in charge of creating all instances of the Country Class
+    */
+    public static class BuilderInvestors{
+        private String id;
+        private int numberOfShares;
+        private double budget;
+        /*
+       creates the objects builder
+        */
+        public BuilderInvestors(String id, double budget, int numberOfShares){
+
+            this.id = id;
+            this.budget = budget;
+            this.numberOfShares = numberOfShares;
+        }
+
+        /*
+    setting and returning an instance of the class from the builder
+     */
+            /*
+        generates ramdon prices for shares
+         */
+        public double ramdomBudget(){
+
+            Random r = new Random();
+            int low = 500;
+            int high = 1000;
+            int budget = r.nextInt(high-low + 1) + low;
+
+            return budget;
+        }
+        /*
+        adding one to the number of shares for every time a share
+        is bought and returning this number
+         */
+        public int buyShares(){
+            numberOfShares = numberOfShares +1;
+            return numberOfShares;
+        }
+        /*
+               setting and returning an instance of the class from the builder
+                */
+        public Investors build(){
+            return new Investors(this);
+        }
+    }
+}
